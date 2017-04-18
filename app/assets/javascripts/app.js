c715975a5391c0d3e96a3c112caf6a84966697b9
williamjones62 <williamjones62@gmail.com>
2017-04-18 15:38:46 -0400

    install angular aria and animate

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,20 @@
+(function () {
+
+  function config($stateProvider, lockProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        controller: 'HomeController',
+        templateUrl: 'components/home/home.html',
+        controllerAs: 'vm'
+      })
+      .state('login', {
+        url: '/login',
+        controller: 'LoginController',
+        templateUrl: 'components/login/login.html',
+        controllerAs: 'vm'
+      });
+  }
+
+})();
